df8a86a4c8947565aebdbc53d9dcec768f36a187
Dizhan Xue <xuedizhan17@mails.ucas.ac.cn>
2024-04-15 17:14:45 +0800

    Add files via upload

---
+++ evaluation.py
@@ -0,0 +1,43 @@
+import os
+import json
+import random
+
+from metrics import lang_eval, vis_eval, attr_evaluater
+
+
+
+class evaluator():
+    def __init__(self, gts):
+        self.exp_gts = gts
+        self.attr_evaluater = attr_evaluater(gts)
+
+    def evaluate(self, exp_res):
+        scores = lang_eval(self.exp_gts, exp_res)
+        scores['IoU'] = vis_eval(self.exp_gts, exp_res)
+        scores.update(self.attr_evaluater.score(exp_res))
+
+        return scores
+
+
+if __name__ == '__main__':
+    res = json.load(open("MEVQA_explanations.json"))
+
+    gts = json.load(open("dataset/MEGQA_test.json"))
+    gts = {k: gts[k] for k in res}
+    assert res.keys() == gts.keys()
+
+    evalor = evaluator(gts)
+
+    acc = []
+    for k in res.keys():
+        if res[k]['answer'] == gts[k]['answer']:
+            acc.append(1)
+        else:
+            acc.append(0)
+
+    acc = sum(acc) / len(acc)
+    print(f"Accuracy = {acc * 100}")
+
+    scores = evalor.evaluate(res)
+    for k in scores:
+        print(f"{k} = {scores[k]}")
